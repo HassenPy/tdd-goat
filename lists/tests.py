c9775d7071d7c48d1ad88dadf8704c8e7450ccbc
HassenPy <hbt.youtube@yahoo.com>
2015-03-09 14:37:01 +0100

    home_page view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.http import HttpRequest
 from django.core.urlresolvers import resolve
+
 from lists.views import home_page
 
 
@@ -8,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_and_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
