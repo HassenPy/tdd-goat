8ebe1b2192424d82ad2573bacde76cbf8e3e6f8b
HassenPy <hbt.youtube@yahoo.com>
2015-03-09 12:09:15 +0100

    Created a failing test on home_page url mapping and view, then passed it

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolvers_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
