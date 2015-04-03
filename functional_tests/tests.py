6b8d1f312d97eeaf4ae1e54fed4a4572b469eefe
HassenPy <hbt.youtube@yahoo.com>
2015-04-03 22:30:07 +0100

    Test if style applies to input field

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -22,6 +22,29 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                (inputbox.location['x'] + inputbox.size['width'] / 2),
+                512,
+                delta=3
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=3
+        )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Popei uses an online to-do app.
         # He opens the home page
